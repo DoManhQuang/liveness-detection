be2b984e9ac797aa4256c7e60923209d949e45ff
Do Manh Quang <quangdm2@ghtk.internal>
2022-11-05 15:34:03 +0700

    add core/*.py

---
+++ core/model.py
@@ -0,0 +1,141 @@
+from keras import Input
+from keras.layers import Conv2D, Dropout, GlobalAveragePooling2D
+from keras.layers import Dense, BatchNormalization, MaxPooling2D, concatenate
+from keras.layers.pooling import AveragePooling2D
+from keras.models import Model
+
+
+def block_conv_a(x, filter_cnv_a, filter_cnv_b, filter_cnv_c, name="Block_Conv_A"):
+    convA = Conv2D(filter_cnv_a, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same')(x)
+    convA = Conv2D(filter_cnv_a, kernel_size=(1, 1), strides=(1, 1), activation='relu', padding='same')(convA)
+    filter_cnv_a = filter_cnv_a / 2
+    convA = Conv2D(filter_cnv_a, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(convA)
+    convA = Conv2D(filter_cnv_a, kernel_size=(1, 1), strides=(1, 1), activation='relu', padding='same')(convA)
+
+    convB = Conv2D(filter_cnv_b, kernel_size=(5, 5), strides=(2, 2), activation='relu', padding='same')(x)
+    convB = Conv2D(filter_cnv_b, kernel_size=(1, 1), strides=(1, 1), activation='relu', padding='same')(convB)
+    filter_cnv_b = filter_cnv_b / 2
+    convB = Conv2D(filter_cnv_b, kernel_size=(5, 5), strides=(1, 1), activation='relu', padding='same')(convB)
+    convB = Conv2D(filter_cnv_b, kernel_size=(1, 1), strides=(1, 1), activation='relu', padding='same')(convB)
+
+    poolC = MaxPooling2D((2, 2), strides=(2, 2), padding='same')(x)
+    poolC = Conv2D(filter_cnv_c, kernel_size=(1, 1), strides=(1, 1), activation='relu', padding='same')(poolC)
+    filter_cnv_c = filter_cnv_c / 2
+    poolC = Conv2D(filter_cnv_c, kernel_size=(1, 1), strides=(1, 1), activation='relu', padding='same')(poolC)
+    poolC = BatchNormalization()(poolC)
+
+    output = concatenate([convA, convB, poolC], name=name)
+    return output
+
+
+def block_conv_b(x, filter_cnv_a, filter_cnv_b, filter_cnv_c, name="Block_Conv_B"):
+    convA = Conv2D(filter_cnv_a, kernel_size=(5, 5), strides=(2, 2), activation='relu', padding='same')(x)
+    convA = Conv2D(filter_cnv_a, kernel_size=(1, 1), strides=(1, 1), activation='relu', padding='same')(convA)
+    filter_cnv_a = filter_cnv_a / 2
+    convA = Conv2D(filter_cnv_a, kernel_size=(1, 1), strides=(1, 1), activation='relu', padding='same')(convA)
+    convA = BatchNormalization()(convA)
+
+    poolB = AveragePooling2D((2, 2), strides=(2, 2), padding='same')(x)
+    poolB = Conv2D(filter_cnv_b, kernel_size=(1, 1), strides=(1, 1), activation='relu', padding='same')(poolB)
+    filter_cnv_b = filter_cnv_b / 2
+    poolB = Conv2D(filter_cnv_b, kernel_size=(1, 1), strides=(1, 1), activation='relu', padding='same')(poolB)
+
+    poolC = AveragePooling2D((2, 2), strides=(2, 2), padding='same')(x)
+
+    output = concatenate([convA, poolB, poolC], name=name)
+    return output
+
+
+def block_identity_a(x, filter_cnv_a, filter_cnv_b, name="Block_Identity_A"):
+    convA = Conv2D(filter_cnv_a, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(x)
+    convA = Conv2D(filter_cnv_a, kernel_size=(1, 1), strides=(1, 1), activation='relu', padding='same')(convA)
+    filter_cnv_a = filter_cnv_a / 2
+    convA = Conv2D(filter_cnv_a, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(convA)
+    convA = Conv2D(filter_cnv_a, kernel_size=(1, 1), strides=(1, 1), activation='relu', padding='same')(convA)
+
+    convB = Conv2D(filter_cnv_b, kernel_size=(5, 5), strides=(1, 1), activation='relu', padding='same')(x)
+    convB = Conv2D(filter_cnv_b, kernel_size=(1, 1), strides=(1, 1), activation='relu', padding='same')(convB)
+    filter_cnv_b = filter_cnv_b / 2
+    convB = Conv2D(filter_cnv_b, kernel_size=(5, 5), strides=(1, 1), activation='relu', padding='same')(convB)
+    convB = Conv2D(filter_cnv_b, kernel_size=(1, 1), strides=(1, 1), activation='relu', padding='same')(convB)
+    convB = BatchNormalization()(convB)
+
+    output = concatenate([convA, convB, x], name=name)
+
+    return output
+
+
+def block_identity_b(x, filter_cnv_a, filter_cnv_b, name="Block_Identity_B"):
+    convA = Conv2D(filter_cnv_a, kernel_size=(5, 5), strides=(1, 1), activation='relu', padding='same')(x)
+    convA = Conv2D(filter_cnv_a, kernel_size=(1, 1), strides=(1, 1), activation='relu', padding='same')(convA)
+    filter_cnv_a = filter_cnv_a / 2
+    convA = Conv2D(filter_cnv_a, kernel_size=(1, 1), strides=(1, 1), activation='relu', padding='same')(convA)
+    convA = BatchNormalization()(convA)
+
+    convB = Conv2D(filter_cnv_b, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(x)
+    filter_cnv_b = filter_cnv_b / 2
+    convB = Conv2D(filter_cnv_b, kernel_size=(1, 1), strides=(1, 1), activation='relu', padding='same')(convB)
+
+    output = concatenate([convA, convB, x], name=name)
+    return output
+
+
+def block_stem(x, filter_cnv_a, name="Block_Stem"):
+    convA = Conv2D(filter_cnv_a, kernel_size=(7, 7), strides=(1, 1), activation='relu', padding='same')(x)
+    convA = Conv2D(filter_cnv_a, kernel_size=(1, 1), strides=(1, 1), activation='relu', padding='same')(convA)
+    convA = MaxPooling2D((2, 2))(convA)
+
+    filter_cnv_a = filter_cnv_a / 2
+
+    convA = Conv2D(filter_cnv_a, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(convA)
+    convA = Conv2D(filter_cnv_a, kernel_size=(1, 1), strides=(1, 1), activation='relu', padding='same', name=name)(convA)
+    return convA
+
+
+def created_model_medium(input_layer, name="Model_Medium"):
+    xS = block_stem(input_layer, filter_cnv_a=256, name="Block_Stem")
+    xS = MaxPooling2D((2, 2), name="MaxPooling2D_S")(xS)
+
+    xA = block_conv_a(xS, filter_cnv_a=128, filter_cnv_b=128, filter_cnv_c=128, name="Block_Conv_A_1")
+    xA = block_identity_a(xA, filter_cnv_a=64, filter_cnv_b=64, name="Block_Identity_A_1")
+    xA = MaxPooling2D((2, 2))(xA)
+    xA = block_conv_a(xA, filter_cnv_a=32, filter_cnv_b=32, filter_cnv_c=32, name="Block_Conv_A_2")
+    xA = block_identity_a(xA, filter_cnv_a=8, filter_cnv_b=8, name="Block_Identity_A_2")
+
+    xB = block_conv_b(xS, filter_cnv_a=128, filter_cnv_b=128, filter_cnv_c=128, name="Block_Conv_B_1")
+    xB = block_identity_b(xB, filter_cnv_a=64, filter_cnv_b=64, name="Block_Identity_B_1")
+    xB = MaxPooling2D((2, 2))(xB)
+    xB = block_conv_b(xB, filter_cnv_a=32, filter_cnv_b=32, filter_cnv_c=32, name="Block_Conv_B_2")
+    xB = block_identity_b(xB, filter_cnv_a=8, filter_cnv_b=8, name="Block_Identity_B_2")
+
+    output = concatenate([xA, xB], name=name)
+    return output
+
+
+def created_model_small(input_layer, name="Model_small"):
+    xS = block_stem(input_layer, filter_cnv_a=256, name="Block_Stem")
+    xS = AveragePooling2D((2, 2), name="AveragePooling2D_1")(xS)
+
+    #     xA = block_conv_a(xS, filter_cnv_a=128, filter_cnv_b=128, filter_cnv_c=128, name="Block_Conv_A_1")
+    #     xA = block_identity_a(xA, filter_cnv_a=64, filter_cnv_b=64, name="Block_Identity_A_1")
+    #     xA = MaxPooling2D((2, 2))(xA)
+    #     xA = block_conv_a(xA, filter_cnv_a=32, filter_cnv_b=32, filter_cnv_c=32, name="Block_Conv_A_2")
+    #     xA = block_identity_a(xA, filter_cnv_a=8, filter_cnv_b=8, name="Block_Identity_A_2")
+
+    xB = block_conv_b(xS, filter_cnv_a=32, filter_cnv_b=32, filter_cnv_c=32, name="Block_Conv_B_1")
+    xB = block_identity_b(xB, filter_cnv_a=8, filter_cnv_b=8, name="Block_Identity_B_1")
+    #     xB = MaxPooling2D((2, 2))(xB)
+    #     xB = block_conv_b(xB, filter_cnv_a=32, filter_cnv_b=32, filter_cnv_c=32, name="Block_Conv_B_2")
+    #     xB = block_identity_b(xB, filter_cnv_a=8, filter_cnv_b=8, name="Block_Identity_B_2")
+
+    return xB
+
+
+def model_classification(input_layer, num_class=2, activation='softmax'):
+    input_layer = Input(shape=input_layer)
+    x = created_model_small(input_layer)
+    x = GlobalAveragePooling2D()(x)
+    x = Dropout(0.5)(x)
+    x = Dense(num_class, activation=activation)(x)
+    model = Model(inputs=input_layer, outputs=x)
+    return model
---
+++ core/utils.py
@@ -0,0 +1,187 @@
+import os
+import pickle
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sn
+import tensorflow as tf
+from keras.callbacks import ModelCheckpoint
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from tqdm import tqdm
+import numpy as np
+from keras.utils import img_to_array, load_img
+
+
+def save_dump(file_path, data, labels):
+    file = open(file_path, 'wb')
+    # dump information to that file
+    pickle.dump((data, labels), file)
+    # close the file
+    file.close()
+    pass
+
+
+def load_data(path_file):
+    file = open(path_file, 'rb')
+    # dump information to that file
+    (pixels, labels) = pickle.load(file)
+    # close the file
+    file.close()
+    print(pixels.shape)
+    print(labels.shape)
+    return pixels, labels
+
+
+def load_data_image_directory(path_folder_data="../dataset", path_labels="labels.csv", img_height=600, img_width=300, cnt_image=5):
+    print("====Start Loading ... ====")
+    data = []
+    labels = []
+
+    labels_csv = np.array(pd.read_csv(path_labels, usecols=["fname", "liveness_score"]))
+    for i in tqdm(range(0, len(labels_csv))):
+        folder_name = labels_csv[i][0].split(".")[0]
+        folder_frame_path = os.path.join(path_folder_data, folder_name)
+        lst_frame = os.listdir(folder_frame_path)
+        cnt = 0
+        for frame in lst_frame:
+            if cnt == cnt_image:
+                break
+            image = load_img(os.path.join(folder_frame_path, frame), target_size=(img_height, img_width))  # (img_height, img_width)
+            image = img_to_array(image)
+            image = tf.image.rgb_to_grayscale(image).numpy()
+            data.append(image)
+            labels.append(labels_csv[i][1])
+
+    data = np.array(data)
+    labels = np.array(labels)
+
+    data_train, data_test, labels_train, labels_test = train_test_split(data, labels, test_size=0.2, shuffle=True,
+                                                                        stratify=labels, random_state=10000)
+
+    print("====End Done !!! ====")
+    return data_train, data_test, labels_train, labels_test
+
+
+def get_callbacks_list(diractory,
+                       status_tensorboard=True,
+                       status_checkpoint=True,
+                       status_earlystop=True,
+                       file_ckpt="ghtk-spamham-weights-best-training-file.hdf5",
+                       ckpt_monitor='val_accuracy',
+                       ckpt_mode='max',
+                       early_stop_monitor="val_accuracy",
+                       early_stop_mode="max",
+                       early_stop_patience=5):
+    callbacks_list = []
+    save_path = []
+    if status_earlystop:
+        # Early Stopping
+        callback_early_stop = tf.keras.callbacks.EarlyStopping(monitor=early_stop_monitor, patience=early_stop_patience,
+                                                               restore_best_weights=True, mode=early_stop_mode, verbose=1)
+        callbacks_list.append(callback_early_stop)
+
+    # create checkpoint
+    if status_checkpoint:
+        checkpoint_path = os.path.join(diractory, "checkpt")
+        if not os.path.exists(checkpoint_path):
+            os.makedirs(checkpoint_path)
+        # file="weights-improvement-{epoch:02d}-{val_accuracy:.2f}.hdf5"
+        file_path = os.path.join(checkpoint_path, file_ckpt)
+        save_path.append(file_path)
+
+        checkpoint_callback = ModelCheckpoint(file_path, monitor=ckpt_monitor, verbose=1,
+                                              save_best_only=True, mode=ckpt_mode, save_weights_only=True)
+        callbacks_list.append(checkpoint_callback)
+
+    # Tensorflow Board
+    if status_tensorboard:
+        tensorboard_path = os.path.join(diractory, "tensorboard-logs")
+        if not os.path.exists(tensorboard_path):
+            os.makedirs(tensorboard_path)
+        tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=tensorboard_path)
+        callbacks_list.append(tensorboard_callback)
+        save_path.append(tensorboard_path)
+
+    return callbacks_list, save_path
+
+
+def plot_model_history(model_history, acc='accuracy', val_acc='val_accuracy'):
+    fig, axs = plt.subplots(1, 2, figsize=(15, 5))
+    axs[0].plot(range(1, len(model_history.history[acc]) + 1), model_history.history[acc])
+    axs[0].plot(range(1, len(model_history.history[val_acc]) + 1), model_history.history[val_acc])
+    axs[0].set_title('Model Accuracy')
+    axs[0].set_ylabel('Accuracy')
+    axs[0].set_xlabel('Epoch')
+    axs[0].set_xticks(np.arange(1, len(model_history.history[acc]) + 1), len(model_history.history[acc]) / 10)
+    axs[0].legend(['train', 'val'], loc='best')
+    axs[1].plot(range(1, len(model_history.history['loss']) + 1), model_history.history['loss'])
+    axs[1].plot(range(1, len(model_history.history['val_loss']) + 1), model_history.history['val_loss'])
+    axs[1].set_title('Model Loss')
+    axs[1].set_ylabel('Loss')
+    axs[1].set_xlabel('Epoch')
+    axs[1].set_xticks(np.arange(1, len(model_history.history['loss']) + 1), len(model_history.history['loss']) / 10)
+    axs[1].legend(['train', 'val'], loc='best')
+    plt.show()
+    # plt.savefig('roc.png')
+
+
+def plot_model_legend(model_history):
+    # view
+    accuracy = model_history.history['accuracy']
+    val_accuracy = model_history.history['val_accuracy']
+    loss = model_history.history['loss']
+    val_loss = model_history.history['val_loss']
+    epochs = range(len(accuracy))
+    plt.plot(epochs, accuracy, 'b', label='Training accuracy')
+    plt.plot(epochs, val_accuracy, 'r', label='Validation accuracy')
+    plt.plot(epochs, loss, 'y', label='Training loss')
+    plt.plot(epochs, val_loss, 'g', label='Validation loss')
+    plt.title('Training and validation')
+    plt.xlim(0, len(accuracy))
+    plt.ylim(0, 1)
+    plt.legend(loc='best')
+    plt.show()
+
+
+def print_cmx(y_true, y_pred):
+    labels = sorted(list(set(y_true)))
+    cmx_data = confusion_matrix(y_true, y_pred, labels=labels)
+    df_cmx = pd.DataFrame(cmx_data, index=labels, columns=labels)
+    plt.figure(figsize=(10, 7))
+    sn.heatmap(df_cmx, annot=True, fmt='g')
+    print(cmx_data)
+    plt.show()
+
+
+def save_results_to_csv(dict_results, directory="./results", name="predict", version="version-0.0"):
+    df_res = pd.DataFrame(data=dict_results)
+    file_name = name + "-" + version + "-result.csv"
+    df_res.to_csv(os.path.join(directory, file_name), encoding="utf-8")
+    print("SAVE DONE")
+    pass
+
+
+def write_score(path="test.txt", mode_write="a", rows="STT", cols=[1.0, 2.0, 3.0]):
+    file = open(path, mode_write)
+    file.write(str(rows) + "*")
+    for col in cols:
+        file.write(str(col) + "*")
+    file.write("\n")
+    file.close()
+    pass
+
+
+def set_gpu_limit(set_memory=5):
+    memory_limit = set_memory * 1024
+    gpus = tf.config.list_physical_devices('GPU')
+    if gpus:
+        # Restrict TensorFlow to only allocate 1GB of memory on the first GPU
+        try:
+            tf.config.set_logical_device_configuration(
+                gpus[0],
+                [tf.config.LogicalDeviceConfiguration(memory_limit=memory_limit)])
+            logical_gpus = tf.config.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+        except RuntimeError as e:
+            # Virtual devices must be set before GPUs have been initialized
+            print(e)
