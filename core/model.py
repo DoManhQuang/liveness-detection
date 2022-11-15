f38bdaa85eae511daa3dc5bd4fe0fd3d44cf2ecc
Do Manh Quang <quangdm2@ghtk.internal>
2022-11-15 13:58:13 +0700

    update model basse

--- core/model.py
+++ core/model.py
@@ -1,9 +1,10 @@
-from keras import Input
 from keras.layers import Conv2D, Dropout, GlobalAveragePooling2D, GlobalMaxPooling2D
+from keras.applications.mobilenet_v2 import MobileNetV2
+from keras import Input
+from keras.layers import Conv2D, Dropout, GlobalAveragePooling2D
 from keras.layers import Dense, BatchNormalization, MaxPooling2D, concatenate
 from keras.layers.pooling import AveragePooling2D
 from keras.models import Model
-from keras.applications.mobilenet_v2 import MobileNetV2
 
 
 def block_conv_a(x, filter_cnv_a, filter_cnv_b, filter_cnv_c, name="Block_Conv_A"):
@@ -24,7 +25,6 @@ def block_conv_a(x, filter_cnv_a, filter_cnv_b, filter_cnv_c, name="Block_Conv_A
     filter_cnv_c = filter_cnv_c / 2
     poolC = Conv2D(filter_cnv_c, kernel_size=(1, 1), strides=(1, 1), activation='relu', padding='same')(poolC)
     poolC = BatchNormalization()(poolC)
-
     output = concatenate([convA, convB, poolC], name=name)
     return output
 
@@ -115,31 +115,19 @@ def created_model_medium(input_layer, name="Model_Medium"):
 
 def created_model_small(input_layer, name="Model_small"):
     xS = block_stem(input_layer, filter_cnv_a=256, name="Block_Stem")
-    xS = MaxPooling2D((2, 2), name="MaxPooling2D_1")(xS)
-
-    #     xA = block_conv_a(xS, filter_cnv_a=128, filter_cnv_b=128, filter_cnv_c=128, name="Block_Conv_A_1")
-    #     xA = block_identity_a(xA, filter_cnv_a=64, filter_cnv_b=64, name="Block_Identity_A_1")
-    #     xA = MaxPooling2D((2, 2))(xA)
-    #     xA = block_conv_a(xA, filter_cnv_a=32, filter_cnv_b=32, filter_cnv_c=32, name="Block_Conv_A_2")
-    #     xA = block_identity_a(xA, filter_cnv_a=8, filter_cnv_b=8, name="Block_Identity_A_2")
-
+    xS = AveragePooling2D((2, 2), name="AveragePooling2D")(xS)
     xB = block_conv_b(xS, filter_cnv_a=32, filter_cnv_b=32, filter_cnv_c=32, name="Block_Conv_B_1")
-    xB = block_identity_b(xB, filter_cnv_a=8, filter_cnv_b=8, name=name)
-    #     xB = MaxPooling2D((2, 2))(xB)
-    #     xB = block_conv_b(xB, filter_cnv_a=32, filter_cnv_b=32, filter_cnv_c=32, name="Block_Conv_B_2")
-    #     xB = block_identity_b(xB, filter_cnv_a=8, filter_cnv_b=8, name="Block_Identity_B_2")
-
+    xB = block_identity_b(xB, filter_cnv_a=8, filter_cnv_b=8, name="Block_Identity_B_1")
     return xB
 
 
-def model_classification(input_layer, num_class=2, activation='softmax'):
+def model_classification(input_layer, num_class=1, activation='sigmoid'):
     input_layer = Input(shape=input_layer)
     x = created_model_small(input_layer)
-    x = GlobalMaxPooling2D()(x)
+    x = GlobalAveragePooling2D()(x)
     x = Dropout(0.5)(x)
     x = Dense(num_class, activation=activation)(x)
-    model = Model(inputs=input_layer, outputs=x)
-    return model
+    return Model(inputs=input_layer, outputs=x)
 
 
 def model_mobile_v2_fine_tune(input_shape=(224, 224, 3), num_class=2, activation='softmax'):
@@ -154,3 +142,5 @@ def model_mobile_v2_fine_tune(input_shape=(224, 224, 3), num_class=2, activation
     return Model(inputs=base_model.input, outputs=x)
 
 
+# model = model_classification(input_layer=(300, 100, 3), num_class=1, activation='sigmoid')
+# model.summary()
