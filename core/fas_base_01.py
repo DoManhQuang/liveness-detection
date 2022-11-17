680502f696fdd0673664f948b14f49c9c9ac024f
Do Manh Quang <quangdm2@ghtk.internal>
2022-11-17 15:06:54 +0700

    update fas

--- core/fas_base_01.py
+++ core/fas_base_01.py
@@ -39,14 +39,17 @@ def block_conv(x, filter_block, activation='relu', padding='same', name="block_c
     pass
 
 
-def block_identity(x, filter_block, kernel_size_a=3, kernel_size_b=1, stride=(1, 1), activation='relu', padding='same', name="block_identity"):
+def block_identity(x, filter_block, kernel_size_a=5, kernel_size_b=3, kernel_size_c=1, stride=1, activation='relu', padding='same', name="block_identity"):
     conv_a = Conv2D(filter_block, kernel_size=kernel_size_a, strides=stride, activation=activation, padding=padding)(x)
     conv_a = BatchNormalization()(conv_a)
 
     conv_b = Conv2D(filter_block, kernel_size=kernel_size_b, strides=stride, activation=activation, padding=padding)(x)
     conv_b = BatchNormalization()(conv_b)
 
-    conv_concat = concatenate([conv_a, conv_b, x])
+    conv_c = Conv2D(filter_block, kernel_size=kernel_size_c, strides=stride, activation=activation, padding=padding)(x)
+    conv_c = BatchNormalization()(conv_c)
+
+    conv_concat = concatenate([conv_a, conv_b, conv_c, x])
     output_block = Activation('relu', name=name)(conv_concat)
     return output_block
 
@@ -61,15 +64,22 @@ def block_identity_group(x, filter_blocks, activation='relu', padding='same', na
 
 def created_model_fas_01(input_shape, number_class, activation):
     input_layer = Input(shape=input_shape)
-    x = block_stem(x=input_layer, filter_cnv=64, pool_size=(2, 2), kernel_size=7, strides=1, activation='relu', padding='same', name="Block_Stem")
-    x = block_conv(x=x, filter_block=64, activation='relu', padding='same', name="block_conv_a")
-    x = block_identity_group(x=x, filter_blocks=[64, 16], activation='relu', padding='same', name="block_identity_group_a")
-    x = GlobalAveragePooling2D()(x)
+    x_stem = block_stem(x=input_layer, filter_cnv=64, pool_size=(2, 2), kernel_size=5, strides=1, activation='relu', padding='same', name="Block_Stem")
+    # x = block_conv(x=x, filter_block=32, activation='relu', padding='same', name="block_conv_a")
+    # x = block_conv(x=x, filter_block=64, activation='relu', padding='same', name="block_conv_a")
+    # x = block_identity_group(x=x, filter_blocks=[64, 16], activation='relu', padding='same', name="block_identity_group_a")
+    x_base_a = block_identity(x_stem, filter_block=64, activation='relu', padding='same', name="block_identity_index_1")
+    x_base_b = block_identity(x_base_a, filter_block=32, activation='relu', padding='same', name="block_identity_index_2")
+    x_base_c = block_identity(x_base_b, filter_block=16, activation='relu', padding='same', name="block_identity_index_3")
+
+    conv_concat = concatenate([x_base_a, x_base_b, x_base_c], name="concat_base")
+
+    x = GlobalAveragePooling2D()(conv_concat)
     x = Dropout(0.5)(x)
     x = Dense(number_class, activation=activation)(x)
     return Model(inputs=input_layer, outputs=x)
 
 
-# model = created_model_fas_01(input_shape=(300, 100, 3), number_class=1, activation='sigmoid')
-# model.summary(show_trainable=True)
+model = created_model_fas_01(input_shape=(300, 100, 3), number_class=1, activation='sigmoid')
+model.summary(show_trainable=True)
 
