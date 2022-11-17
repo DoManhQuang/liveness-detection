8c8e8879e4c2d0ad30ed784359c762b9c8fbf05c
Do Manh Quang <quangdm2@ghtk.internal>
2022-11-17 16:09:14 +0700

    update fas-v1

--- core/fas_base_01.py
+++ core/fas_base_01.py
@@ -66,20 +66,20 @@ def created_model_fas_01(input_shape, number_class, activation):
     input_layer = Input(shape=input_shape)
     x_stem = block_stem(x=input_layer, filter_cnv=64, pool_size=(2, 2), kernel_size=5, strides=1, activation='relu', padding='same', name="Block_Stem")
     # x = block_conv(x=x, filter_block=32, activation='relu', padding='same', name="block_conv_a")
-    # x = block_conv(x=x, filter_block=64, activation='relu', padding='same', name="block_conv_a")
+    x_conv = block_conv(x=x_stem, filter_block=64, activation='relu', padding='same', name="block_conv_a")
     # x = block_identity_group(x=x, filter_blocks=[64, 16], activation='relu', padding='same', name="block_identity_group_a")
-    x_base_a = block_identity(x_stem, filter_block=64, activation='relu', padding='same', name="block_identity_index_1")
+    x_base_a = block_identity(x_conv, filter_block=64, activation='relu', padding='same', name="block_identity_index_1")
     x_base_b = block_identity(x_base_a, filter_block=32, activation='relu', padding='same', name="block_identity_index_2")
     x_base_c = block_identity(x_base_b, filter_block=16, activation='relu', padding='same', name="block_identity_index_3")
 
     conv_concat = concatenate([x_base_a, x_base_b, x_base_c], name="concat_base")
 
-    x = GlobalAveragePooling2D()(conv_concat)
+    x = Flatten()(conv_concat)
     x = Dropout(0.5)(x)
     x = Dense(number_class, activation=activation)(x)
     return Model(inputs=input_layer, outputs=x)
 
 
-model = created_model_fas_01(input_shape=(300, 100, 3), number_class=1, activation='sigmoid')
-model.summary(show_trainable=True)
+# model = created_model_fas_01(input_shape=(300, 100, 3), number_class=1, activation='sigmoid')
+# model.summary(show_trainable=True)
 
